14417cd77a1582017b23a0f6c0b3068fe1fed86c
Tommaso De Tommaso <detommaso23@gmail.com>
2022-10-16 12:16:42 +0200

    Fixed some comments and deleted repeated lines

--- Sett2-Es1/main.c
+++ Sett2-Es1/main.c
@@ -5,63 +5,62 @@ int main()
 {
     int a = 10, b = 5, c;
 
-    c = a + b; // Somma
+    // Operazioni base
+
+    // Somma
+    c = a + b;
     printf("Somma: %d + %d = %d\n", a, b, c);
 
-    c = a - b; // Differenza
+    // Differenza
+    c = a - b;
     printf("Differenza: %d - %d = %d\n", a, b, c);
 
-    c = a * b; // Moltiplicazione
+    // Moltiplicazione
+    c = a * b;
     printf("Moltiplicazione: %d * %d = %d\n", a, b, c);
 
-    c = a / b; // Divisione
+    // Divisione
+    c = a / b;
     printf("Divisione: %d / %d = %d\n", a, b, c);
 
-    c = a % b; // Modulo
+    // Modulo
+    c = a % b;
     printf("Modulo: %d %% %d = %d\n", a, b, c);
 
-    // Incremento sul suffisso
-    printf("Incremento: %d++ = %d\n", ++a, a);
-    // Decremento sul suffisso
-    printf("Decremento: %d-- = %d\n", --a, a);
-
-    // Incremento sul prefisso
-    printf("Incremento sul prefisso: ++%d = %d\n", a, ++a);
-    // Decremento sul prefisso
-    printf("Decremento sul prefisso: --%d = %d\n", a, --a);
-
+    // Confronti
 
     if (a == b) 
-    {   // Se a è uguale a b
-        printf("%d e uguale a %d\n", a, b);
+    {
+        printf("%d e' uguale a %d\n", a, b);
     }
 
     if (a != b) 
-    {   // Se a è diverso da b
-        printf("%d e diverso da %d\n", a, b);
+    {
+        printf("%d e' diverso da %d\n", a, b);
     }
 
     if (a > b) 
     {
-        printf("%d e maggiore di %d\n", a, b);
+        printf("%d e' maggiore di %d\n", a, b);
     }
 
     if (a < b)
     {
-        printf("%d e minore di %d\n", a, b);
+        printf("%d e' minore di %d\n", a, b);
     }
 
     if (a >= b) 
     {
-        printf("%d e maggiore o uguale a %d\n", a, b);
+        printf("%d e' maggiore o uguale a %d\n", a, b);
     }
 
     if (a <= b)
     {
-        printf("%d e minore o uguale %d\n", a, b);
+        printf("%d e' minore o uguale %d\n", a, b);
     }
 
 
+    // Assegnazione con operazioni
 
     c = a;
     c += c;
@@ -85,72 +84,69 @@ int main()
 
 
     c = a++;
-    printf("facendo c = a++ c ottiene il valore di a prima che gli venga sommato 1 infatti c = %d a= %d\n",c,a);
+    printf("Con c = a++, c ottiene il valore di a prima che gli venga sommato 1, infatti c = %d, a = %d\n", c, a);
     
     c = --a;
-    printf("facendo c = --a prima viene tolto 1 ad a poi il valore viene dato a c, infatti c = %d a= %d\n",c,a);
+    printf("Con c = --a, prima viene tolto 1 ad a, poi il valore viene dato a c, infatti c = %d, a = %d\n", c, a);
 
-    c =2147483648;
-    printf("inserendo un valore troppo alto all'interno di una variabile causiamo un overflow, in questo caso il numero restituito equivale al numero piu basso + l'overflow ovvero %d\n",c);
+    c = 2147483648;
+    printf("Superando il limite massimo di una variabile causiamo un overflow, in questo caso il numero che conterrà equivale al numero piu' basso + l'overflow ovvero %d\n", c);
 
-    c= 10.1853f;
-    printf("cercando di inserire il valore 10.1853 dentro una variabile int otteniamo : %d",c);
+    c = 10.1853f;
+    printf("Cercando di inserire il valore 10.1853 dentro una variabile intera otteniamo %d", c);
 
 
-    //funzioni di math.h
+    // Funzioni di math.h
 
     double d;
 
     d = sqrt(4.0);
-    printf("la funzione  sqrt(4) restituisce= %f\n",d);
+    printf("La funzione sqrt(4) restituisce %f\n", d);
 
     d = sin(1.0);
-    printf("la funzione sin(1) restituisce  %f\n",d);
+    printf("La funzione sin(1) restituisce %f\n", d);
 
     d = cos(1.0);
-    printf("la funzione cos(1) restituisce  %f\n",d);
+    printf("La funzione cos(1) restituisce %f\n", d);
 
     d = tan(1.0);
-    printf("la funzione tan(1) restituisce  %f\n",d);
+    printf("La funzione tan(1) restituisce %f\n", d);
 
     d = asin(0.7);
-    printf("la funzione asin(0.7)restituisce  %f\n",d);
+    printf("La funzione asin(0.7)restituisce %f\n", d);
 
     d = acos(0.0);
-    printf("la funzione acos(0) restituisce  %f\n",d);
+    printf("La funzione acos(0) restituisce %f\n", d);
 
     d = atan(45.0);
-    printf("la funzione atan(45) restituisce  %f\n",d);
+    printf("La funzione atan(45) restituisce %f\n", d);
 
     d = exp(1.0);
-    printf("la funzione e(1) restituisce  %f\n",d);
+    printf("La funzione e(1) restituisce %f\n", d);
     
     /*d = pow10(1.0);
-    printf("la funzione pow10(1) restituisce  %f",d);*/
+    printf("La funzione pow10(1) restituisce %f", d);*/
 
     d = log(1.0);
-    printf("la funzione log(1) restituisce  %f\n",d);
+    printf("La funzione log(1) restituisce %f\n", d);
 
     d = log10(1.0);
-    printf("la funzione log10(1) restituisce  %f\n",d);
+    printf("La funzione log10(1) restituisce %f\n", d);
 
     d = fabs(-1.0);
-    printf("la funzione fabs(-1) restituisce  %f\n",d);
+    printf("La funzione fabs(-1) restituisce %f\n", d);
 
     d = hypot(1.0,0.0);
-    printf("la funzione hypot(1,0) restituisce  %f\n",d);
+    printf("La funzione hypot(1,0) restituisce %f\n", d);
 
     d = atan2(1.0,1.0);
-    printf("la funzione atan2(1,1) restituisce  %f\n",d);
+    printf("La funzione atan2(1,1) restituisce %f\n", d);
 
     d = pow(5.0,1.0);
-    printf("la funzione pow(5,1) restituisce  %f\n",d);
+    printf("La funzione pow(5,1) restituisce %f\n", d);
 
-    d = frexp(2.0,&a);
-    printf("la funzione frexp(2,&a) restituisce  %f\n",d);
+    d = frexp(2.0, &a);
+    printf("La funzione frexp(2,%d) restituisce %f\n", a, d);
     
-    return 0;
-    
-    
-    
+    return 0;    
 }
