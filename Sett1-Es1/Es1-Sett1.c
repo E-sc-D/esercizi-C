02411aa6c4b9b57f28da5cf59ec6005aeb416aa6
E-sc-D <scorzaedoardo@gmail.com>
2022-10-16 16:02:55 +0200

    flag #,0

--- Sett1-Es1/Es1-Sett1.c
+++ Sett1-Es1/Es1-Sett1.c
@@ -44,41 +44,41 @@ int main(void)
     //Avendo creato una tabella usando i campi [flags] e [larghezza] di printf, il numero di caratteri stampati sarà costante per ogni riga.
 
     printf("%-25s | %-25s | %-25s | %-25s | %-25s | %-25s\n", "Tipologia variabile", "Contenuto", "Byte necessari", "Massimo valore", "Minimo valore", "Caratteri stampati");
-    caratteriStampati = printf("%-25s | %-25c | %-25d | %-25d | %-25d | ", "CHAR", carattere, sizeof(char), CHAR_MAX, CHAR_MIN);
+    caratteriStampati = printf("%-25s | %-25c | %025d | %-25d | %-#25d | ", "CHAR", carattere, sizeof(char), CHAR_MAX, CHAR_MIN);
+    printf("%0-25d\n", caratteriStampati);
+    caratteriStampati = printf("%-25s | %-+25c | %025d | %-25d | %-25hd | ", "SIGNED CHAR", carattereConSegno, sizeof(signed char), SCHAR_MAX, SCHAR_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-+25c | %-25d | %-25d | %-25hd | ", "SIGNED CHAR", carattereConSegno, sizeof(signed char), SCHAR_MAX, SCHAR_MIN);
+    caratteriStampati = printf("%-25s | %-25c | %025d | %-25d | %-25d | ", "UNSIGNED CHAR", carattereSenzaSegno, sizeof(unsigned char), UCHAR_MAX, 0);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25c | %-25d | %-25d | %-25d | ", "UNSIGNED CHAR", carattereSenzaSegno, sizeof(unsigned char), UCHAR_MAX, 0);
+    caratteriStampati = printf("%-25s | %-+25hd | %025d | %-25hd | %-25hd | ", "SHORT INT", interoCorto, sizeof(short int), SHRT_MAX, SHRT_MIN);//*
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-+25hd | %-25d | %-25hd | %-25hd | ", "SHORT INT", interoCorto, sizeof(short int), SHRT_MAX, SHRT_MIN);//*
+    caratteriStampati = printf("%-25s | %-+25d | %025d | %-25d | %-25d | ", "INT", intero, sizeof(int), INT_MAX, INT_MIN);//*
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-+25d | %-25d | %-25d | %-25d | ", "INT", intero, sizeof(int), INT_MAX, INT_MIN);//*
+    caratteriStampati = printf("%-25s | %-+25ld | %025d | %-25ld | %-25ld | ", "LONG INT", interoLungo, sizeof(long int), LONG_MAX, LONG_MIN);//*
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-+25ld | %-25d | %-25ld | %-25ld | ", "LONG INT", interoLungo, sizeof(long int), LONG_MAX, LONG_MIN);//*
+    caratteriStampati = printf("%-25s | %-+25lld | %025d | %-25lld | %-25lld | ", "LONG LONG INT", interoMoltoLungo, sizeof(long long int), LLONG_MAX, LLONG_MIN);//*
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-+25lld | %-25d | %-25lld | %-25lld | ", "LONG LONG INT", interoMoltoLungo, sizeof(long long int), LLONG_MAX, LLONG_MIN);//*
+    caratteriStampati = printf("%-25s | %-25hd | %025d | %-25hd | %-25hd | ", "SIGNED SHORT INT", interoCortoConSegno, sizeof(signed short int), SHRT_MAX, SHRT_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25hd | %-25d | %-25hd | %-25hd | ", "SIGNED SHORT INT", interoCortoConSegno, sizeof(signed short int), SHRT_MAX, SHRT_MIN);
+    caratteriStampati = printf("%-25s | %-25d | %025d | %-25d | %-25d | ", "SIGNED INT", interoConSegno, sizeof(signed int), INT_MAX, INT_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25d | %-25d | %-25d | %-25d | ", "SIGNED INT", interoConSegno, sizeof(signed int), INT_MAX, INT_MIN);
+    caratteriStampati = printf("%-25s | %-25ld | %025d | %-25ld | %-25ld | ", "SIGNED LONG INT", interoLungoConSegno, sizeof(signed long int), LONG_MAX, LONG_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25ld | %-25d | %-25ld | %-25ld | ", "SIGNED LONG INT", interoLungoConSegno, sizeof(signed long int), LONG_MAX, LONG_MIN);
+    caratteriStampati = printf("%-25s | %-25lld | %025d | %-25lld | %-25lld | ", "SIGNED LONG LONG INT", interoMoltoLungoConSegno, sizeof(signed long long int), LLONG_MAX, LLONG_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25lld | %-25d | %-25lld | %-25lld | ", "SIGNED LONG LONG INT", interoMoltoLungoConSegno, sizeof(signed long long int), LLONG_MAX, LLONG_MIN);
+    caratteriStampati = printf("%-25s | %-25hu | %025d | %-25hu | %-25hu | ", "UNSIGNED SHORT INT", interoCortoSenzaSegno, sizeof(unsigned short int), USHRT_MAX, 0);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25hu | %-25d | %-25hu | %-25hu | ", "UNSIGNED SHORT INT", interoCortoSenzaSegno, sizeof(unsigned short int), USHRT_MAX, 0);
+    caratteriStampati = printf("%-25s | %-25u | %025d | %-25u | %-25u | ", "UNSIGNED INT", interoSenzaSegno, sizeof(unsigned int), UINT_MAX, 0);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25u | %-25d | %-25u | %-25u | ", "UNSIGNED INT", interoSenzaSegno, sizeof(unsigned int), UINT_MAX, 0);
+    caratteriStampati = printf("%-25s | %-25lu | %025d | %-25lu | %-25lu | ", "UNSIGNED LONG INT", interoLungoSenzaSegno, sizeof(unsigned long int), ULONG_MAX, 0);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25lu | %-25d | %-25lu | %-25lu | ", "UNSIGNED LONG INT", interoLungoSenzaSegno, sizeof(unsigned long int), ULONG_MAX, 0);
+    caratteriStampati = printf("%-25s | %-25llu | %025d | %-25llu | %-25llu | ", "UNSIGNED LONG LONG INT", interoMoltoLungoSenzaSegno, sizeof(unsigned long long int), ULLONG_MAX, 0);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25llu | %-25d | %-25llu | %-25llu | ", "UNSIGNED LONG LONG INT", interoMoltoLungoSenzaSegno, sizeof(unsigned long long int), ULLONG_MAX, 0);
+    caratteriStampati = printf("%-25s | %-#25.15f | %025d | %-25.5e | %-25.5e | ", "FLOAT", decimale, sizeof(float), FLT_MAX, FLT_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25.15f | %-25d | %-25.5e | %-25.5e | ", "FLOAT", decimale, sizeof(float), FLT_MAX, FLT_MIN);
+    caratteriStampati = printf("%-25s | %-25.15f | %025d | %-25.5e | %-25.5e | ", "DOUBLE", decimalePreciso, sizeof(double), DBL_MAX, DBL_MIN);
     printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25.15f | %-25d | %-25.5e | %-25.5e | ", "DOUBLE", decimalePreciso, sizeof(double), DBL_MAX, DBL_MIN);
-    printf("%-25d\n", caratteriStampati);
-    caratteriStampati = printf("%-25s | %-25.15Lf | %-25d | %-25.5Le | %-25.5Le | ", "LONG DOUBLE", decimalePrecisoLungo, sizeof(long double), LDBL_MAX, LDBL_MIN);
+    caratteriStampati = printf("%-25s | %-25.15Lf | %025d | %-25.5Le | %-25.5Le | ", "LONG DOUBLE", decimalePrecisoLungo, sizeof(long double), LDBL_MAX, LDBL_MIN);
     printf("%-25d\n", caratteriStampati);
 
     return 0;
