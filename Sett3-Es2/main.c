288919ecd815677dc0c42a87e8e553f122b84fa6
Tommaso De Tommaso <detommaso23@gmail.com>
2022-10-19 14:23:37 +0200

    Continued work on Sett3-Es2... because I don't know how to continue the exercise I should be doing. El Psy Kongroo

--- Sett3-Es2/main.c
+++ Sett3-Es2/main.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
 #include <ctype.h>
+
+
 
 int main()
 {
     /*
-     * Finché getch funzioni, bisogna aprire direttamente l'eseguibile (problema console CLion)
-     * */
-    char c;
-    int contAlfaNum; // numero di caratteri alfanumerici
-    int contAlf; // numero di caratteri alfabetici
-    int contControllo; // numero di caratteri di controllo
-    int contCifre; // numero di caratteri cifre (0-9)
-    int contGraf; // numero di caratteri con rappresentazione grafica
-    int contMinuscoli; // numero di caratteri minuscoli
-    int contStampabile; // numero di caratteri stampabili (non di controllo)
-    int contPunteggiatura; // numero caratteri di punteggiatura
-    int contSpazi; // numero di caratteri spazio
+        Affinché getch funzioni, bisogna aprire direttamente l'eseguibile (problema console CLion)
+    */
 
-    printf("Inserisci un testo seguito da un '#' per indicarne la fine e ti stampero le statistiche\n");
+    char c;
+
+    int contTotale = 0; // Numero di caratteri inseriti
+    int contAlfaNum = 0; // Numero di caratteri alfanumerici
+    int contAlf = 0; // Numero di caratteri alfabetici
+    int contControllo = 0; // Numero di caratteri di controllo
+    int contCifre = 0; // Numero di caratteri cifre (0-9)
+    int contGraf = 0; // Numero di caratteri con rappresentazione grafica
+    int contMinuscoli = 0; // Numero di caratteri minuscoli
+    int contMaiuscoli = 0; // Numero di caratteri maiuscoli
+    int contStampabile = 0; // Numero di caratteri stampabili (non di controllo)
+    int contPunteggiatura = 0; // Numero caratteri di punteggiatura
+    int contSpazi = 0; // Numero di caratteri spazio
+
+    printf("Inserisci un testo seguito da un '#' per indicarne la fine e ti stampero' le statistiche\n");
 
     do {
-        c = getch();
+        c = getc(stdin);
+        contTotale++;
 
         if (isalnum(c))
             contAlfaNum++;
@@ -41,32 +47,24 @@ int main()
             contPunteggiatura++;
         if (isspace(c))
             contSpazi++;
-
-
-
-
-
-
+        if (isupper(c))
+            contMaiuscoli++;
+        //if (isxdigit(c))
+        //    contCifre++;
     }
     while (c != '#');
 
-    if (isspace(c)) {
-        printf("Il carattere %c e uno spazio", c);
-    } else {
-        printf("Il carattere %c non e uno spazio", c);
-    }
-
-    if (isupper(c)) {
-        printf("Il carattere %c e una maiuscola", c);
-    } else {
-        printf("Il carattere %c non e una maiuscola", c);
-    }
-
-    if (isxdigit(c)) {
-        printf("Il carattere %c e esadecimale", c);
-    } else {
-        printf("Il carattere %c non e esadecimale", c);
-    }
+    printf("Il numero totale di caratteri inseriti e' %d\n", contTotale);
+    printf("Il numero di caratteri alfanumerici inseriti e' %d\n", contAlfaNum);
+    printf("Il numero di caratteri alfabetici inseriti e' %d\n", contAlf);
+    printf("Il numero di caratteri di controllo inseriti e' %d\n", contControllo);
+    printf("Il numero di caratteri cifra inserite e' %d\n", contCifre);
+    printf("Il numero di caratteri con rappresentazione grafica inseriti e' %d\n", contGraf);
+    printf("Il numero di caratteri minuscoli inseriti e' %d\n", contMinuscoli);
+    printf("Il numero di caratteri maiuscoli inseriti e' %d\n", contMaiuscoli);
+    printf("Il numero di caratteri stampabili (non di controllo) inseriti e' %d\n", contStampabile);
+    printf("Il numero di caratteri di punteggiatura inseriti e' %d\n", contPunteggiatura);
+    printf("Il numero di caratteri spazio inseriti e' %d\n", contSpazi);
 
     return 0;
 }
