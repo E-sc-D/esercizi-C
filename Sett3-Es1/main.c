2621298240ea4b4d7abab101a0a84fe032267024
Tommaso De Tommaso <detommaso23@gmail.com>
2022-10-19 14:53:05 +0200

    Testing sleep() or delaying with time.h

--- Sett3-Es1/main.c
+++ Sett3-Es1/main.c
@@ -8,15 +8,24 @@ struct Point
     double y;
 };
 
+void delay(int seconds)
+{
+    int milliseconds = 1000 * seconds;
+    clock_t startTime = clock();
+    while(clock() < startTime + milliseconds);
+}
+
 int main(void)
 {
-
     srand(time(NULL));
 
     struct Point coords;
     coords.x = rand();
     coords.y = rand();
 
-
-
+    printf("TEST");
+    fflush(stdout);
+    delay(5);
+    //sleep(5);
+    printf("\rBRUH\n");
 }
