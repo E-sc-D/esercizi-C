8ac891386ef6644ecd3e3190e170bf911f041b27
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2022-10-26 20:52:21 +0200

    Polish

--- Sett4-Es1/main.c
+++ Sett4-Es1/main.c
@@ -8,7 +8,7 @@ int main()
 
     int array1[5];
     int array2[10];
-    int i = 0;
+    int i = 0, j = 0;
 
     printf("Inserisci cinque valori interi da tastiera\n");
     for (i = 0; i < 5; i++) {
@@ -20,45 +20,44 @@ int main()
         printf("[%d]", array1[i]);
     }
 
-    printf("\nArray 2: ");
+    printf("\nArray2: ");
     // Stampo e genero gli elementi del secondo array in maniera pseudo-casuale
     for (i = 0; i < 10; i++) {
-        rnd = rand() % 100; // numero random compreso tra 0 e 99
+        rnd = rand() % 10; // numero random compreso tra 0 e 99
+        //printf("%d", rnd);
         array2[i] = rnd;
         printf("[%d]", array2[i]);
     }
 
     // intersezione senza ripetizioni
-    //int arrayIntersezione[5];   // il numero di elementi è 5 perché facendo l'intersezione senza ripetizioni il numero massimo di possibili elementi in comune sarà sempre uguale alla lunghezza dell'array più piccolo (con meno elementi)
-
-    int verifica[5];
-    int k = 0;
+    int verifica[5];    // array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
     for (i = 0; i < 5; i++) {
-        for (int j = 0; j < 10; j++) {
+        verifica[i] = 0;    // per poter lavorare sugli elementi dell'array li inizializzo a 0
+        for (j = 0; j < 10; j++) {
             if (array1[i] == array2[j]) {
-                verifica[i]++;
-                break;
+                verifica[i]++;  // conto l'intersezione
             }
         }
     }
     // print di array verifica
+    printf("\nArray verifica: ");
     for (i = 0; i < 5; i++) {
         printf("[%d]", verifica[i]);
     }
 
 
     int cont = 0;
-    for (int i = 0; i < 5; i++) {
+    // Scorro l'array di verifica e conto il numero di elementi che fanno intersezione, per poter creare l'array finale che contiene solo elementi di intersezione senza ripetizioni
+    for (i = 0; i < 5; i++) {
         if (verifica[i] >= 1) {
             cont++;
         }
     }
+
     int arrayIntersezione[cont];
 
-
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < cont; j++){
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < cont; j++){
             if (verifica[i] >= 1) {
                 arrayIntersezione[j] = array1[i];
             }
@@ -66,7 +65,7 @@ int main()
     }
 
     printf("\nArray intersezione: ");
-    for (int i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
         printf("[%d]", arrayIntersezione[i]);
     }
 
