cb519c083b990bf95cae707b6f687311fc821d9e
Tommaso De Tommaso <detommaso23@gmail.com>
2022-10-30 12:27:58 +0100

    Introduced the coolerIntArray!

--- Sett4-Es1/main.c
+++ Sett4-Es1/main.c
@@ -2,40 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
-// size_t n è il numero di elementi dell'array
-void printArray(int array[], size_t n)
+struct coolerIntArray //Array di int che tiene conto della propria lunghezza
+{
+    int size;
+    int* array;
+};
+
+void printArray(int array[], size_t size)
 {
     int i = 0;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < size; i++)
     {
         printf("[%d]", array[i]);
     }
+    printf("\n");
 }
 
-int* intersezione(const int array1[], const int array2[]) // intersezione senza ripetizioni
+struct coolerIntArray intersezione(const int array1[], const int array2[]) // intersezione senza ripetizioni
 {
     int i = 0, j = 0;
 
-    int contaIntersezioni[5];    // array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
+    int contaIntersezioni[5];    // Array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
     for (i = 0; i < 5; i++)
     {
-        contaIntersezioni[i] = 0;    // per poter lavorare sugli elementi dell'array li inizializzo a 0
+        contaIntersezioni[i] = 0;    // Inizializzazione degli elementi dell'array
         for (j = 0; j < 10; j++)
         {
             if (array1[i] == array2[j])
             {
-                contaIntersezioni[i]++;  // conto l'intersezione
+                contaIntersezioni[i]++;  // Conta l'intersezione (sarà la lunghezza dell'array intersezione)
             }
         }
     }
 
-    // print di array contaIntersezioni
-    printf("\nArray contaIntersezioni: ");
-    printArray(contaIntersezioni, sizeof(contaIntersezioni) / sizeof(contaIntersezioni[0]));
-
     int cont = 0;
     int flag = 0;
-    // Scorro l'array di contaIntersezioni e conto il numero di elementi che fanno intersezione, per poter creare l'array finale che contiene solo elementi di intersezione senza ripetizioni
+    // Scorro l'array di contaIntersezioni e conto il numero di elementi che fanno intersezione,
+    // per poter creare l'array finale che contiene solo elementi di intersezione senza ripetizioni
     for (i = 0; i < 5; i++)
     {
         if (contaIntersezioni[i] >= 1)
@@ -54,7 +57,6 @@ int* intersezione(const int array1[], const int array2[]) // intersezione senza 
             }
         }
     }
-    printf("\nL'array di intersezione avra %d elementi", cont);
 
     int* arrayIntersezione = (int*)malloc(cont * sizeof(int)); //int arrayIntersezione[cont];
 
@@ -79,27 +81,23 @@ int* intersezione(const int array1[], const int array2[]) // intersezione senza 
 
         }
     }
-    printf("\nArray intersezione: ");
-    printArray(arrayIntersezione, cont);
 
-    // Questi sono i valori di return (perche per lavorare sugli array devo anche sapere quanti elementi hanno per non sforare
-    //*size = cont;
-    //*array = arrayIntersezione;
+    struct coolerIntArray coolerArrayIntersezione;
+    coolerArrayIntersezione.size = cont;
+    coolerArrayIntersezione.array = arrayIntersezione;
 
-    //printf("\n*arrayIntersezione %d", *arrayIntersezione);
-    //printf("\nIndirizzo memoria di arrayIntersezione %d", arrayIntersezione);
-
-    return arrayIntersezione;
+    return coolerArrayIntersezione;
 }
 
 
 int main()
 {
     srand(time(NULL));
-    int rnd = 0;    // inizializzo la variabile che conterrà il numero random
+    int rnd = 0;    // Inizializzo la variabile che conterrà il numero random
 
     int array1[5];
     int array2[10];
+
     int i = 0, j = 0;
 
     printf("Inserisci cinque valori interi da tastiera\n");
@@ -108,75 +106,23 @@ int main()
         scanf("%d", &array1[i]);
     }
     printf("Array1: ");
-    printArray(array1, sizeof(array1)/sizeof(array1[0]));   // Stampo gli elementi del primo array  sizeof(.. calcola il numero di elementi dell'array
+    printArray(array1, sizeof(array1)/sizeof(array1[0]));   // Stampo gli elementi del primo array calcolandone il numero di elementi
 
-
-    printf("\nArray2: ");
-    // Stampo e genero gli elementi del secondo array in maniera pseudo-casuale
+    // Genero gli elementi del secondo array in maniera pseudo-casuale
     for (i = 0; i < 10; i++)
     {
-        rnd = rand() % 10; // numero random compreso tra 0 e 9
-        //printf("%d", rnd);
+        rnd = rand() % 10; // Numero random compreso tra 0 e 9
         array2[i] = rnd;
-        printf("[%d]", array2[i]);
     }
+    printf("Array2: ");
+    printArray(array2, sizeof(array2)/sizeof(array2[0])); // Stampo gli elementi del secondo array
 
-    int size;
-    int* p;
-    int* arrayIntersezione = intersezione(array1, array2); //, &size, &p);
-
-    /*// intersezione senza ripetizioni
-    int contaIntersezioni[5];    // array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
-    for (i = 0; i < 5; i++) {
-        contaIntersezioni[i] = 0;    // per poter lavorare sugli elementi dell'array li inizializzo a 0
-        for (j = 0; j < 10; j++) {
-            if (array1[i] == array2[j])
-                contaIntersezioni[i]++;  // conto l'intersezione
-        }
-    }
-    // print di array contaIntersezioni
-    printf("\nArray contaIntersezioni: ");
-    printArray(contaIntersezioni, sizeof(contaIntersezioni) / sizeof(contaIntersezioni[0]));
-
-
-    int cont = 0;
-    int flag = 0;
-    // Scorro l'array di contaIntersezioni e conto il numero di elementi che fanno intersezione, per poter creare l'array finale che contiene solo elementi di intersezione senza ripetizioni
-    for (i = 0; i < 5; i++) {
-        if (contaIntersezioni[i] >= 1) {
-            flag = 0;
-            for (j = i - 1; j >= 0; j--){
-                if (array1[i] == array1[j]) {
-                    flag = 1;
-                }
-            }
-            if (flag == 0)
-                cont++;
-        }
-    }
-    printf("\nL'array di intersezione avra %d elementi", cont);
-
-    int arrayIntersezione[cont];
-    int k = 0;
-    for (i = 0; i < 5; i++) {
-        if (contaIntersezioni[i] >= 1) {
-            flag = 0;
-            for (j = i - 1; j >= 0; j--){
-                if (array1[i] == array1[j]) {
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                arrayIntersezione[k] = array1[i];
-                k++;
-            }
-
-        }
-    }
+    struct coolerIntArray arrayIntersezione = intersezione(array1, array2); //, &size, &p);
 
     printf("\nArray intersezione: ");
-    printArray(arrayIntersezione, sizeof(arrayIntersezione)/sizeof(arrayIntersezione[0]));
-*/
+    printArray(arrayIntersezione.array, arrayIntersezione.size);
+
+    free(arrayIntersezione.array); // arrayIntersezione è stato creato con malloc quindi deve essere de-allocato manualmente
     return 0;
 }
 
