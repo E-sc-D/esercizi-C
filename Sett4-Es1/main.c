9916dbf5db9f89d7dbad58570d02ef0bbbf5d16a
Tommaso De Tommaso <detommaso23@gmail.com>
2022-10-30 13:18:51 +0100

    Added intersezione con ripetizioni e unione

--- Sett4-Es1/main.c
+++ Sett4-Es1/main.c
@@ -2,33 +2,58 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printArray(int array[], size_t size)
-{
-    int i = 0;
-    for (i = 0; i < size; i++)
-    {
-        printf("[%d]", array[i]);
-    }
-    printf("\n");
-}
-
 struct coolerIntArray //Array di int che tiene conto della propria lunghezza
 {
     int size;
     int* array;
 };
 
-struct coolerIntArray intersezione(const int array1[], const int array2[]) // intersezione senza ripetizioni
+void printCoolerIntArray(const struct coolerIntArray arrayToPrint)
 {
+    int i = 0;
+    for (i = 0; i < arrayToPrint.size; i++)
+    {
+        printf("[%d]", arrayToPrint.array[i]);
+    }
+    printf("\n");
+}
+
+void printIntArray(const int arrayToPrint[], const size_t size)
+{
+    int i = 0;
+    for (i = 0; i < size; i++)
+    {
+        printf("[%d]", arrayToPrint[i]);
+    }
+    printf("\n");
+}
+
+struct coolerIntArray intersezioneSenzaRipetizioni(const struct coolerIntArray array1, const struct coolerIntArray array2)
+{
+    struct coolerIntArray arrayMaggiore;
+    struct coolerIntArray arrayMinore;
+
+    // Determinare quale array ha la lunghezza minore e quale la lunghezza maggiore
+    if(array1.size > array2.size) // I puntatori arrayMaggiore e arrayMinore, punteranno rispettivamente array1 e array2
+    {
+        arrayMaggiore = array1;
+        arrayMinore = array2;
+    }
+    else // I puntatori punteranno il contrario di quanto accade sopra
+    {
+        arrayMaggiore = array2;
+        arrayMinore = array1;
+    }
+
     int i = 0, j = 0;
 
-    int contaIntersezioni[5];    // Array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
-    for (i = 0; i < 5; i++)
+    int contaIntersezioni[arrayMinore.size];    // Array che terrà conto di quante volte un elemento compare anche nell'altro array (intersezione)
+    for (i = 0; i < arrayMinore.size; i++)
     {
         contaIntersezioni[i] = 0;    // Inizializzazione degli elementi dell'array
-        for (j = 0; j < 10; j++)
+        for (j = 0; j < arrayMaggiore.size; j++)
         {
-            if (array1[i] == array2[j])
+            if (arrayMinore.array[i] == arrayMaggiore.array[j])
             {
                 contaIntersezioni[i]++;  // Conta l'intersezione (sarà la lunghezza dell'array intersezione)
             }
@@ -39,14 +64,14 @@ struct coolerIntArray intersezione(const int array1[], const int array2[]) // in
     int flag = 0;
     // Scorro l'array di contaIntersezioni e conto il numero di elementi che fanno intersezione,
     // per poter creare l'array finale che contiene solo elementi di intersezione senza ripetizioni
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < arrayMinore.size; i++)
     {
         if (contaIntersezioni[i] >= 1)
         {
             flag = 0;
             for (j = i - 1; j >= 0; j--)
             {
-                if (array1[i] == array1[j])
+                if (arrayMinore.array[i] == arrayMinore.array[j])
                 {
                     flag = 1;
                 }
@@ -61,21 +86,21 @@ struct coolerIntArray intersezione(const int array1[], const int array2[]) // in
     int* arrayIntersezione = (int*)malloc(cont * sizeof(int)); //int arrayIntersezione[cont];
 
     int k = 0;
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < arrayMinore.size; i++)
     {
         if (contaIntersezioni[i] >= 1)
         {
             flag = 0;
             for (j = i - 1; j >= 0; j--)
             {
-                if (array1[i] == array1[j])
+                if (arrayMinore.array[i] == arrayMinore.array[j])
                 {
                     flag = 1;
                 }
             }
             if (flag == 0)
             {
-                arrayIntersezione[k] = array1[i];
+                arrayIntersezione[k] = arrayMinore.array[i];
                 k++;
             }
 
@@ -85,8 +110,67 @@ struct coolerIntArray intersezione(const int array1[], const int array2[]) // in
     struct coolerIntArray coolerArrayIntersezione;
     coolerArrayIntersezione.size = cont;
     coolerArrayIntersezione.array = arrayIntersezione;
-
     return coolerArrayIntersezione;
+}
+
+struct coolerIntArray intersezioneConRipetizioni(const struct coolerIntArray array1, const struct coolerIntArray array2)
+{
+    int i = 0, j = 0;
+
+    int cont = 0;
+    for(i = 0; i < array1.size; i++)
+    {
+        for(j = 0; j < array2.size; j++)
+        {
+            if(array1.array[i] == array2.array[j])
+            {
+                cont++;
+            }
+        }
+    }
+
+    int* arrayIntersezione = (int*)malloc(cont * sizeof(int)); //int arrayIntersezione[cont];ù
+    
+    int k = 0;
+    for(i = 0; i < array1.size; i++)
+    {
+        for(j = 0; j < array2.size; j++)
+        {
+            if(array1.array[i] == array2.array[j])
+            {
+                arrayIntersezione[k] = array1.array[i];
+                k++;
+            }
+        }
+    }
+
+    struct coolerIntArray coolerArrayIntersezione;
+    coolerArrayIntersezione.size = cont;
+    coolerArrayIntersezione.array = arrayIntersezione;
+    return coolerArrayIntersezione;
+}
+
+struct coolerIntArray unione(const struct coolerIntArray array1, const struct coolerIntArray array2)
+{
+    int unioneSize = array1.size + array2.size;
+    int* arrayUnione = (int*)malloc(unioneSize * sizeof(int)); //int arrayUnione[cont];
+
+    int i = 0, k = 0;
+    for(i = 0; i < array1.size; i++)
+    {
+        arrayUnione[k] = array1.array[i];
+        k++;
+    }
+    for(i = 0; i < array2.size; i++)
+    {
+        arrayUnione[k] = array2.array[i];
+        k++;
+    }
+
+    struct coolerIntArray coolerArrayUnione;
+    coolerArrayUnione.size = unioneSize;
+    coolerArrayUnione.array = arrayUnione;
+    return coolerArrayUnione;
 }
 
 int main()
@@ -94,34 +178,54 @@ int main()
     srand(time(NULL));
     int rnd = 0;    // Inizializzo la variabile che conterrà il numero random
 
-    int array1[5];
-    int array2[10];
+    const int array1_length = 5;
+    int array1[array1_length];
+    const int array2_length = 10;
+    int array2[array2_length];
+
+    struct coolerIntArray coolerArray1;
+    coolerArray1.size = array1_length;
+    coolerArray1.array = array1;
+
+    struct coolerIntArray coolerArray2;
+    coolerArray2.size = array2_length;
+    coolerArray2.array = array2;
 
     int i = 0, j = 0;
-
-    printf("Inserisci cinque valori interi da tastiera\n");
-    for (i = 0; i < 5; i++)
+    printf("Inserisci dei valori interi da tastiera\n");
+    for (i = 0; i < coolerArray1.size; i++)
     {
-        scanf("%d", &array1[i]);
+        scanf("%d", &coolerArray1.array[i]);
     }
     printf("Array1: ");
-    printArray(array1, sizeof(array1)/sizeof(array1[0]));   // Stampo gli elementi del primo array calcolandone il numero di elementi
+    printCoolerIntArray(coolerArray1);   // Stampo gli elementi del primo array calcolandone il numero di elementi
 
     // Genero gli elementi del secondo array in maniera pseudo-casuale
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < coolerArray2.size; i++)
     {
         rnd = rand() % 10; // Numero random compreso tra 0 e 9
-        array2[i] = rnd;
+        coolerArray2.array[i] = rnd;
     }
     printf("Array2: ");
-    printArray(array2, sizeof(array2)/sizeof(array2[0])); // Stampo gli elementi del secondo array
+    printCoolerIntArray(coolerArray2); // Stampo gli elementi del secondo array
 
-    struct coolerIntArray arrayIntersezione = intersezione(array1, array2); //, &size, &p);
+    struct coolerIntArray coolerArrayResult;
+    
+    coolerArrayResult = intersezioneSenzaRipetizioni(coolerArray1, coolerArray2); //, &size, &p);
+    printf("Array intersezione senza ripetizioni: ");
+    printCoolerIntArray(coolerArrayResult);
+    free(coolerArrayResult.array); // coolerArrayResult.array è stato creato originariamente con malloc,
+                                   // quindi la memoria deve essere liberata manualmente
 
-    printf("\nArray intersezione: ");
-    printArray(arrayIntersezione.array, arrayIntersezione.size);
+    coolerArrayResult = intersezioneConRipetizioni(coolerArray1, coolerArray2);
+    printf("Array intersezione con ripetizioni: ");
+    printCoolerIntArray(coolerArrayResult);
+    free(coolerArrayResult.array);
 
-    free(arrayIntersezione.array); // arrayIntersezione è stato creato originariamente con malloc, quindi la memoria deve essere liberata manualmente
-    // Tuttavia il programma termina successivamente, quindi è facoltativo in questo caso, ma rimane una buona pratica.
+    coolerArrayResult = unione(coolerArray1, coolerArray2);
+    printf("Array unione: ");
+    printCoolerIntArray(coolerArrayResult);
+    free(coolerArrayResult.array);
+
     return 0;
 }
