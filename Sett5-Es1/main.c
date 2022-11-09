f02c0d9622f1671a6f879b4686176be65d2466c2
Leonardo Grimaldi - leonardo.grimaldi2@studio.unibo.it <leonardo.grimaldi2@studio.unibo.it>
2022-11-09 11:31:40 +0100

    Started es1 sett5

---
+++ Sett5-Es1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void FabargeEgg() {
+    float r1, r2, r3;   // tre numeri reali
+    float omega;
+
+    omega =
+
+
+}
+
+float generaFloat(float min, float max) {
+    float scale = rand() / (float) RAND_MAX;
+}
+
+int main() {
+    srand(time(NULL));
+
+    FabargeEgg();
+
+    return 0;
+}
+
