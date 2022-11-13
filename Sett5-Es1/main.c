8cf784511b6976dbe5f0bc4e801d89bd259fd7fe
Tommaso De Tommaso <detommaso23@gmail.com>
2022-11-13 09:45:45 +0100

    Made everything more compact

--- Sett5-Es1/main.c
+++ Sett5-Es1/main.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 // articolo https://muse.jhu.edu/article/607790/pdf
+
 float generaFloat(float min, float max)
 {
     float scale = rand() / (float) RAND_MAX;
@@ -13,21 +14,24 @@ float generaFloat(float min, float max)
     return randomFloat;
 }
 
-void FabargeEgg(int matrixHeight, int matrixWidth, int resultMatrix[matrixHeight][matrixWidth])
+void FabargeEgg(int resultMatrix[3][400][400])
 {
-    int i = 0, j = 0;
-    float pulsazione = generaFloat(0.15, 0.80);
-    float fasePhi = generaFloat(0.0, 1.0);
-    float fasePsi = generaFloat(0.0, 1.0);
-
-    for (i = 0; i < matrixHeight; i++)
+    int i, j, k;
+    for(k = 0; k < 3; k++)
     {
-        for (j = 0; j < matrixWidth; j++)
+        float pulsazione = generaFloat(0.15, 0.80);
+        float fasePhi = generaFloat(0.0, 1.0);
+        float fasePsi = generaFloat(0.0, 1.0);
+
+        for (i = 0; i < 400; i++)
         {
-            float Fji = sin(pulsazione * i + fasePhi) + sin(pulsazione * j + fasePsi);
-            float mu = generaFloat(0.0, 20.0);
-            int beta = generaFloat(128, 256);
-            resultMatrix[i][j] = ((int)floor((beta + mu)*(1.0 + Fji / 2.0))) % beta;
+            for (j = 0; j < 400; j++)
+            {
+                float Fji = sin(pulsazione * i + fasePhi) + sin(pulsazione * j + fasePsi);
+                float mu = generaFloat(0.0, 20.0);
+                int beta = generaFloat(128, 256);
+                resultMatrix[k][i][j] = ((int)floor((beta + mu)*(1.0 + Fji / 2.0))) % beta;
+            }
         }
     }
 }
@@ -36,47 +40,23 @@ int main()
 {
     srand(time(NULL));
 
-    int rows = 20;
-    int columns = 20;
+    int matrix[3][400][400];
+    FabargeEgg(matrix);
 
-    int matrix1[rows][columns];
-    FabargeEgg(rows, columns, matrix1);
-    int matrix2[rows][columns];
-    FabargeEgg(rows, columns, matrix2);
-    int matrix3[rows][columns];
-    FabargeEgg(rows, columns, matrix3);
-    // No fourth matrix as it's not needed (also it would be an horrible movie)
-
-    int i, j;
-    printf("Printing first matrix\n");
-    for (i = 0; i < rows; i++)
+    int i, j, k;
+    for(k = 0; k < 3; k++)
     {
-        for (j = 0; j < columns; j++)
+        printf("Printing matrix number %d\n", k + 1);
+        for (i = 0; i < 400; i++)
         {
-            printf(" %d ", matrix1[i][j]);
+            for (j = 0; j < 400; j++)
+            {
+                printf(" %d ", matrix[k][i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-
-    printf("Printing second matrix\n");
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j < columns; j++)
-        {
-            printf(" %d ", matrix2[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Printing third matrix\n");
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j < columns; j++)
-        {
-            printf(" %d ", matrix3[i][j]);
-        }
-        printf("\n");
-    }
+    
 
     // ############################################################# //
     // Is this the end? Does it do what it's supposed to? Who knows? //
