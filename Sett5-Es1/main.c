d1e68532a00d53da35d147fd6c375b1c3ef1457f
Tommaso De Tommaso <detommaso23@gmail.com>
2022-11-12 16:38:46 -0800

    I did something? I think...?

--- Sett5-Es1/main.c
+++ Sett5-Es1/main.c
@@ -1,40 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <math.h>
 
 // articolo https://muse.jhu.edu/article/607790/pdf
-float generaFloat(float min, float max) {
+float generaFloat(float min, float max)
+{
     float scale = rand() / (float) RAND_MAX;
 
     float randomFloat = min + scale * (max - min);
-    printf("Numero float generator: %.2f", randomFloat);
+    //printf("Numero float generator: %.2f", randomFloat); // For debugging purposes
     return randomFloat;
 }
 
-void FabargeEgg() {
-    float matrice[400][400];
-
+void FabargeEgg(int matrixHeight, int matrixWidth, int resultMatrix[matrixHeight][matrixWidth])
+{
     int i = 0, j = 0;
-    float pulsazione;
-    float fasePhi, fasePsi;
-    pulsazione = generaFloat(0.15, 0.80);
-    fasePhi = generaFloat(0.0, 1.0);
-    fasePsi = generaFloat(0.0, 1.0);
+    float pulsazione = generaFloat(0.15, 0.80);
+    float fasePhi = generaFloat(0.0, 1.0);
+    float fasePsi = generaFloat(0.0, 1.0);
 
-    for (i = 0; i < 400; i++) {
-        for (j = 0; j < 400; j++) {
-            //matrice[i,j] = 1 + 0.5;
+    for (i = 0; i < matrixHeight; i++)
+    {
+        for (j = 0; j < matrixWidth; j++)
+        {
+            float Fji = sin(pulsazione * i + fasePhi) + sin(pulsazione * j + fasePsi);
+            float mu = generaFloat(0.0, 20.0);
+            int beta = generaFloat(128, 256);
+            resultMatrix[i][j] = ((int)floor((beta + mu)*(1.0 + Fji / 2.0))) % beta;
         }
     }
-
-
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
 
-    FabargeEgg();
+    int rows = 20;
+    int columns = 20;
+
+    int matrix1[rows][columns];
+    FabargeEgg(rows, columns, matrix1);
+    int matrix2[rows][columns];
+    FabargeEgg(rows, columns, matrix2);
+    int matrix3[rows][columns];
+    FabargeEgg(rows, columns, matrix3);
+    // No fourth matrix as it's not needed (also it would be an horrible movie)
+
+    int i, j;
+    printf("Printing first matrix\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf(" %d ", matrix1[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Printing second matrix\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf(" %d ", matrix2[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Printing third matrix\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf(" %d ", matrix3[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
