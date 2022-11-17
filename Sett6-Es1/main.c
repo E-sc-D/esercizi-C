954da827ab5ca7a6a6cfb13246c903fc9fc78797
Tommaso De Tommaso <detommaso23@gmail.com>
2022-11-17 23:19:58 +0100

    Changed focus back on Rubrica rather than Menu

--- Sett6-Es1/main.c
+++ Sett6-Es1/main.c
@@ -1,39 +1,18 @@
 #include <stdio.h>
 #include "rubrica.c"
-#include "interactableMenu.c"
-
-void function1()
-{
-    printf("xd");
-}
-
-void function2()
-{
-    printf("lol");
-}
 
 int main()
 {
-    /*struct Rubrica rubrica; // = NewRubrica(); Technically needed, but apparently not (detailed explaination in "rubrica.c")
+    struct Rubrica rubrica; // = NewRubrica(); Technically needed, but apparently not (detailed explaination in "rubrica.c")
 
-    if(IsRubricaFull(rubrica))
+    if(IsRubricaFull(&rubrica))
     {
         printf("Full");
     }
     else
     {
         printf("Not full");
-    }*/
-
-    //struct MenuStyle menuStyle = DefaultMenuStyle;
-    struct MenuStyle menuStyle = NewMenuStyle('#', 20, 9, NewMenuSpacing(1, 2, 1, 2), NewMenuSpacing(1, 2, 1, 2), 1);
-    struct MenuOption menuOptions[2] =
-    {
-        NewMenuOption("Test", &function1),
-        NewMenuOption("Test2", &function2)
-    };
-    struct Menu menu = NewMenu(2, menuOptions, menuStyle);
-    DrawMenu(menu);
+    }
 
     return 0;
 }
