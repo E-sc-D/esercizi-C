31a98f51599a6e1c93da3db79ab21b51308fc569
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2022-11-22 11:25:49 +0100

    Problema con loading

--- Sett7-Es1/main.c
+++ Sett7-Es1/main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 void robConnect();
-
-int main() {
-
-    robConnect();
-
-
-    return 0;
-}
 
 struct Stanza {
     char nome[20];
@@ -19,10 +13,36 @@ struct Stanza {
     int priorita;
 };
 
-void robConnect() {
+int main() {
+
+    int numero_stanze = 5;
+    //robConnect();    // connettiti al robot (loading)
+
+
+    return 0;
+}
+
+void robConnect() { // Funzione che simula l'effetto di caricamento
     printf("\t\tConnessione a Rob8");
     printf("\n\t\t");
 
+    int percentage = 0;
     char loading[] = "--------------------";
-    printf("%s", loading);
+    printf("%s %d%%", loading, percentage);
+    for(int i = 0; i < strlen(loading); i++) {
+        //printf("\33[2K\r");
+        loading[i] = 'o';
+        percentage = (i + 1) * 5;
+        printf("\n\t\t");
+        printf("%s %d%%", loading, percentage);
+        usleep(50*1000);    // 50 milisecondi (50*1000 microsecondi)
+    }
+}
+
+void aggiungiStanza() {
+
+}
+
+void mostraInfoStanza(char nomeStanza[]) {
+
 }
