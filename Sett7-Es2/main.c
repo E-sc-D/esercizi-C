680caefee1432c71fa0eb65eacf25c6cc46296da
Leonardo Grimaldi - leonardo.grimaldi2@studio.unibo.it <leonardo.grimaldi2@studio.unibo.it>
2022-11-30 12:50:57 +0100

    aggiunto qualche metodo

--- Sett7-Es2/main.c
+++ Sett7-Es2/main.c
@@ -6,14 +6,20 @@
 #include <ctype.h>
 
 void aggiungiStudente();
+void mostraInfoStudente();
+int verificaDuplicatoStudente();
+void azzeraStructStudente();
 
 struct Studente {
+    // Non usare 0 come numero matricola!!
+    // 0 è il numero usato per indicare NULL ovvero che lo Studente non è ancora stato inserito/lo spazio è libero
     int numero_matricola;
     char nome[20];
     char cognome[20];
     int anno_immatricolazione;
 
-
+    int numero_esami;
+    struct PianoStudi *ptrPianoStudi;   // Questo puntatore conterrà l'array di strutture PianoStudi con indice num_esami, che verrà generato all'aggiunta dello studente
 };
 
 struct Insegnamento {
@@ -24,7 +30,7 @@ struct Insegnamento {
 
 };
 
-struct Esame {  // rivedere forse il nome di questa struct
+struct PianoStudi {  // rivedere forse il nome di questa struct
     int codice_insegnamento;
     int voto;
 };
@@ -34,15 +40,71 @@ int main () {
     int numero_studenti = 10;
 
     struct Studente *ptrStudente = (struct Studente *) malloc(numero_studenti * sizeof(struct Studente));
-    //azzeraStruct()?
 
+    azzeraStructStudente(ptrStudente, numero_studenti);
 
 
 
     return 0;
 }
 
-void aggiungiStudente(int numero_matricola) {
+void aggiungiStudente(struct Studente *ptrStudente, int numero_studenti) {
+    int indiceStudente = 0;
+    int flag = 0;
+    for (indiceStudente = 0; indiceStudente < numero_studenti; indiceStudente++) {
+        if (ptrStudente[indiceStudente].numero_matricola == 0) {
+            flag = 1;   // trovato un posto libero di indice "indiceStudente"
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("Attenzione: il vettore di studenti e completo. Non si possono aggiungere ulteriori studenti");
+        printf("\nPremi invio per tornare al menu principale");
+        getch();
+        return;
+    }
+
+    struct Studente s;
+    printf("Inserisci il numero matricola dello studente\n\r");
+    scanf("%d", s.numero_matricola);
+
+    while(s.numero_matricola == 0) {
+        printf("\nAttenzione! Non puoi inserire 0 come numero matricola. Riprova"); // 0 corrisponde a null
+
+        printf("Inserisci il numero matricola dello studente\n\r");
+        scanf("%d", s.numero_matricola);
+    }
+
+    if (verificaDuplicatoStudente(*ptrStudente, numero_studenti, s.numero_matricola) == 1) {
+        printf("\nErrore: lo studente inserito e gia presente! Premi invio per tornare al menu principale");
+        getch();
+        return;
+    }
+
+    printf("\nInserisci il nome dello studente\n\r");
+    scanf("%s", &s.nome);
+
+    printf("\nInserisci il cognome dello studente\n\r");
+    scanf("%s", &s.cognome);
+
+    printf("\nInserisci l'anno di immatricolazione dello studente\n\r");
+    scanf("%d", &s.anno_immatricolazione);
+
+    printf("\nInserisci il numero di esami dello studente\n\r");
+    scanf("%d", &s.numero_esami);
+
+    s.ptrPianoStudi = (struct PianoStudi *) malloc(s.numero_esami * sizeof(struct PianoStudi));
+
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -69,5 +131,20 @@ void mostraInfoStudente(struct Studente *ptrStudente, int numero_studenti, int n
 
     printf("\n\rPremi invio per tornare al menu principale\n\r");
     getch();
+}
 
+int verificaDuplicatoStudente(struct Studente *ptrStudente, int numero_studenti, int numero_matricola) {
+    for (int i = 0; i < numero_studenti; i++) {
+        if (ptrStudente[i].numero_matricola == numero_matricola) {
+            return 1;   // esiste un duplicato
+        }
+    }
+
+    return 0;   // non ci sono duplicati
+}
+
+void azzeraStructStudente(struct Studente *ptrStudente, int numero_studenti) {
+    for(int i = 0; i < numero_studenti; i++) {
+        memset(&ptrStudente[i], 0, sizeof(struct Studente));
+    }
 }
