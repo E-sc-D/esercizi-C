b3119676f7975f501d7cc7be6db010ca54777669
Leonardo Grimaldi - leonardo.grimaldi2@studio.unibo.it <leonardo.grimaldi2@studio.unibo.it>
2022-11-30 11:38:33 +0100

    Aggiunto es2 sett7

---
+++ Sett7-Es2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct Studente {
+    int numero_matricola;
+    char nome[20];
+    char cognome[20];
+
+};
+
+int main () {
+
+
+
+    return 0;
+}
