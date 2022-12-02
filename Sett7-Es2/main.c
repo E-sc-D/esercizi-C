f08e7499ea8f59aa04e7b7b15bba3b810dc10e68
Tommaso De Tommaso <detommaso23@gmail.com>
2022-12-02 16:17:34 +0100

    Updated Sett7-Es2

--- Sett7-Es2/main.c
+++ Sett7-Es2/main.c
@@ -12,7 +12,8 @@ void azzeraStructStudente();
 void generaInsegnamenti();
 void aggiungiPianoStudi();
 
-struct Studente {
+struct Studente
+{
     // Non usare 0 come numero matricola!!
     // 0 è il numero usato per indicare NULL ovvero che lo Studente non è ancora stato inserito/lo spazio è libero
     int numero_matricola;
@@ -24,7 +25,8 @@ struct Studente {
     struct PianoStudi *ptrPianoStudi;   // Questo puntatore conterrà l'array di strutture PianoStudi con indice num_esami, che verrà generato all'aggiunta dello studente
 };
 
-struct Insegnamento {
+struct Insegnamento
+{
     int codice_insegnamento;
     char descrizione[60];
     int anno_somministrazione;
@@ -32,41 +34,49 @@ struct Insegnamento {
 
 };
 
-struct PianoStudi {  // rivedere forse il nome di questa struct
+struct PianoStudi // rivedere forse il nome di questa struct
+{
     int codice_insegnamento;
     int voto;
 };
 
-int main () {
-    int numero_insegnamenti;
-    struct Insegnamento *ptrInsegnamento;
-    generaInsegnamenti(&numero_insegnamenti, &ptrInsegnamento); // inserisco in ptrInsegnamento un array di insegnamenti default (vedi metodo)
-
+int main ()
+{
+    struct Insegnamento insegnamenti[3] =
+    {
+        { 1, "Analisi 1", 2022, 6 },
+        { 2, "Programmazione", 2022, 6 },
+        { 3, "Sistemi operativi", 2022, 6 }
+    };
 
     int numero_studenti = 10;
     struct Studente *ptrStudente = (struct Studente *) malloc(numero_studenti * sizeof(struct Studente));   // creo un array di numero_studenti strutture
 
     azzeraStructStudente(ptrStudente, numero_studenti);
 
-    printf("%d", *ptrStudente);
-    printf("\n%d", ptrStudente);
+    printf("%d\n", *ptrStudente);
+    printf("%d\n", ptrStudente);
 
-    aggiungiStudente(ptrStudente, numero_studenti, ptrInsegnamento, numero_insegnamenti);
+    aggiungiStudente(ptrStudente, numero_studenti, sizeof(insegnamenti) / sizeof(struct Insegnamento), insegnamenti);
 
     return 0;
 }
 
-void aggiungiStudente(struct Studente *ptrStudente, int numero_studenti, struct Insegnamento *ptrInsegnamento, int numero_insegnamenti) {
+void aggiungiStudente(struct Studente *ptrStudente, int numero_studenti, int numero_insegnamenti, struct Insegnamento insegnamenti[numero_insegnamenti])
+{
     int indiceStudente = 0;
     int flag = 0;
-    for (indiceStudente = 0; indiceStudente < numero_studenti; indiceStudente++) {
-        if (ptrStudente[indiceStudente].numero_matricola == 0) {
+    for (indiceStudente = 0; indiceStudente < numero_studenti; indiceStudente++)
+    {
+        if (ptrStudente[indiceStudente].numero_matricola == 0)
+        {
             flag = 1;   // trovato un posto libero di indice "indiceStudente"
             break;
         }
     }
 
-    if (flag == 0) {
+    if (flag == 0)
+    {
         printf("Attenzione: il vettore di studenti e completo. Non si possono aggiungere ulteriori studenti");
         printf("\nPremi invio per tornare al menu principale");
         getch();
@@ -77,14 +87,16 @@ void aggiungiStudente(struct Studente *ptrStudente, int numero_studenti, struct 
     printf("Inserisci il numero matricola dello studente\n\r");
     scanf("%d", &s.numero_matricola);
 
-    while(s.numero_matricola == 0) {
+    while(s.numero_matricola == 0)
+    {
         printf("\nAttenzione! Non puoi inserire 0 come numero matricola. Riprova"); // 0 corrisponde a null
 
         printf("Inserisci il numero matricola dello studente\n\r");
         scanf("%d", &s.numero_matricola);
     }
 
-    if (verificaDuplicatoStudente(ptrStudente, numero_studenti, s.numero_matricola) == 1) {
+    if (verificaDuplicatoStudente(ptrStudente, numero_studenti, s.numero_matricola) == 1)
+    {
         printf("\nErrore: lo studente inserito e gia presente! Premi invio per tornare al menu principale");
         getch();
         return;
@@ -102,44 +114,67 @@ void aggiungiStudente(struct Studente *ptrStudente, int numero_studenti, struct 
     printf("\nInserisci il numero di esami dello studente\n\r");
     scanf("%d", &s.numero_esami);
 
-    s.ptrPianoStudi = (struct PianoStudi *) malloc(s.numero_esami * sizeof(struct PianoStudi));
-    aggiungiPianoStudi(s.ptrPianoStudi, s.numero_esami, ptrInsegnamento, numero_insegnamenti);
+    aggiungiPianoStudi(&s, numero_insegnamenti, insegnamenti);
 
-
-
-
-
-
-
-
-
+    printf("\nStudente aggiunto correttamente!");
 }
 
-void aggiungiPianoStudi(struct PianoStudi *ptrPianoStudi, int numero_esami, struct Insegnamento *ptrInsegnamento, int numero_insegnamenti) {
+void aggiungiPianoStudi(struct Studente *studente, int numero_insegnamenti, struct Insegnamento insegnamenti[numero_insegnamenti])
+{
     system("cls");
-    printf("Piano studi:");
+    printf("Piano studi:\n");
 
-    printf("Scegli l'insegnamento:");
-    for (int i = 0; i < numero_insegnamenti; i++) {
-        printf("\n%d. %s", ptrInsegnamento[i].codice_insegnamento, ptrInsegnamento[i].descrizione);   // esempio: 1. Analisi 1 \n 2. Programmazione \n 3. Sistemi Operativi
+    printf("Insegnamenti disponibili");
+    for (int i = 0; i < numero_insegnamenti; i++)
+        printf("\n%d. %s", insegnamenti[i].codice_insegnamento, insegnamenti[i].descrizione);   // esempio: 1. Analisi 1 \n 2. Programmazione \n 3. Sistemi Operativi
+
+    printf("\n");
+
+    studente->ptrPianoStudi = (struct PianoStudi *) malloc(studente->numero_esami * sizeof(struct PianoStudi));
+    int i = 0;
+    while(i < studente->numero_esami)
+    {
+        printf("\nIterazione n. %d", i);
+        int scelta;
+        do
+        {
+            printf("\nSeleziona un insegnamento da aggiungere al piano studi\n\r");
+            printf("(range valori accettabili: [1, %d])\n\r", numero_insegnamenti);
+            scanf("%d", &scelta);
+        }
+        while(scelta < 1 || scelta > numero_insegnamenti);
+
+        int voto;
+        do
+        {
+            printf("\nInserisci il voto da aggiungere per %s (0 se non ancora svolto)\n\r", insegnamenti[scelta - 1].descrizione);
+            printf("(range valore accettabili: [0, 30])\n\r");
+            scanf("%d", &voto);
+        }
+        while(voto < 0 || voto > 30);
+
+        studente->ptrPianoStudi[i].codice_insegnamento = insegnamenti[scelta - 1].codice_insegnamento;
+        studente->ptrPianoStudi[i].voto = voto;
+
+        i++;
     }
-
-    int scelta;
-    scanf("%d", &scelta);
-
 }
 
-void mostraInfoStudente(struct Studente *ptrStudente, int numero_studenti, int numero_matricola) {
+void mostraInfoStudente(struct Studente *ptrStudente, int numero_studenti, int numero_matricola)
+{
     int indiceStudente = 0;
     int flag = 0;
-    for (indiceStudente = 0; indiceStudente < numero_studenti; indiceStudente++) {
-        if (ptrStudente[indiceStudente].numero_matricola == numero_matricola) {
+    for (indiceStudente = 0; indiceStudente < numero_studenti; indiceStudente++)
+    {
+        if (ptrStudente[indiceStudente].numero_matricola == numero_matricola)
+        {
             flag = 1;
             break;
         }
     }
 
-    if (flag == 0) {
+    if (flag == 0)
+    {
         printf("Non e stato trovato uno studente con il numero di matricola inserito. Premi invio per continuare\r\n");
         getch();
         return;
@@ -154,18 +189,19 @@ void mostraInfoStudente(struct Studente *ptrStudente, int numero_studenti, int n
     getch();
 }
 
-int verificaDuplicatoStudente(struct Studente *ptrStudente, int numero_studenti, int numero_matricola) {
-    for (int i = 0; i < numero_studenti; i++) {
-        if (ptrStudente[i].numero_matricola == numero_matricola) {
+int verificaDuplicatoStudente(struct Studente *ptrStudente, int numero_studenti, int numero_matricola)
+{
+    for (int i = 0; i < numero_studenti; i++)
+        if (ptrStudente[i].numero_matricola == numero_matricola)
             return 1;   // esiste un duplicato
-        }
-    }
 
     return 0;   // non ci sono duplicati
 }
 
-void azzeraStructStudente(struct Studente *ptrStudente, int numero_studenti) {
-    for(int i = 0; i < numero_studenti; i++) {
+void azzeraStructStudente(struct Studente *ptrStudente, int numero_studenti)
+{
+    for(int i = 0; i < numero_studenti; i++)
+    {
         //memset(&ptrStudente[i], 0, sizeof(struct Studente));
         //ptrStudente[i] = (struct Studente){0};
         ptrStudente[i].numero_matricola = 0;
@@ -175,27 +211,4 @@ void azzeraStructStudente(struct Studente *ptrStudente, int numero_studenti) {
         ptrStudente[i].numero_esami = 0;
         ptrStudente[i].ptrPianoStudi = 0;
     }
-}
-
-void generaInsegnamenti(int *numero, int *struttura) {  // passando gli indirizzi delle variabili, questo metodo le riempie con rispettivamente il numero di insegnamenti e l'array di strutture insegnamenti
-    int numero_insegnamenti = 3;
-    struct Insegnamento *ptrInsegnamento = (struct Insegnamento *) malloc(numero_insegnamenti * sizeof(struct Insegnamento));
-
-    ptrInsegnamento[0].codice_insegnamento = 1;
-    strcpy(ptrInsegnamento[0].descrizione, "Analisi 1");
-    ptrInsegnamento[0].anno_somministrazione = 2022;
-    ptrInsegnamento[0].crediti = 6;
-
-    ptrInsegnamento[1].codice_insegnamento = 2;
-    strcpy(ptrInsegnamento[1].descrizione, "Programmazione");
-    ptrInsegnamento[1].anno_somministrazione = 2022;
-    ptrInsegnamento[1].crediti = 6;
-
-    ptrInsegnamento[2].codice_insegnamento = 3;
-    strcpy(ptrInsegnamento[2].descrizione, "Sistemi operativi");
-    ptrInsegnamento[2].anno_somministrazione = 2022;
-    ptrInsegnamento[2].crediti = 6;
-
-    *numero = numero_insegnamenti;
-    *struttura = ptrInsegnamento;
 }
