de04e83e6e7c2365391de086186f0368abf54073
Leonardo Grimaldi - leonardo.grimaldi2@studio.unibo.it <leonardo.grimaldi2@studio.unibo.it>
2022-12-07 14:29:26 +0100

    Small changes

--- Sett9-Es1/main.c
+++ Sett9-Es1/main.c
@@ -5,9 +5,25 @@
 #include <conio.h>
 #include <ctype.h>
 
+struct Giocatore {
+    int posizione; // posizione del giocatore sulla tabella
+
+    char simbolo;   // simbolo con cui verrà identificato il giocatore sulla tavola
+};
+
+struct Casella {
+    int numero; // numero della casella
+    int effetto; // L'effetto può essere andare avanti oppure indietro, per es (+6 posizioni oppure -10). Se è 0 indica che non ha nessun effetto
+};
+
 int main() {
 
 
 
     return 0;
+}
+
+void CreaTabellone() {
+    struct Casella tabellone[100];
+
 }
