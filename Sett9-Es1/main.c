385ba9c8ea274c46d0eb863471e9e31b571bf0d0
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2022-12-08 13:28:32 +0100

    Aggiunta la generazione di serpenti e scale, stampa_tabellone

--- Sett9-Es1/main.c
+++ Sett9-Es1/main.c
@@ -9,6 +9,9 @@
 void crea_tabellone();
 void genera_tabellone();
 int genera_numero_in_range();
+void genera_serpenti();
+void genera_scale();
+void stampa_tabellone();
 
 
 struct Giocatore {
@@ -25,12 +28,18 @@ struct Casella {
 int main() {
     srand(time(NULL));
 
+    crea_tabellone();
+
     return 0;
 }
 
 void crea_tabellone() {
-    int numero_caselle;
-    struct Casella tabellone[100];
+    int numero_caselle = 100;
+    struct Casella tabellone[numero_caselle];
+
+    genera_tabellone(tabellone, numero_caselle);
+
+    stampa_tabellone(tabellone, numero_caselle);
 
 }
 
@@ -43,35 +52,93 @@ void genera_tabellone(struct Casella *tabellone, int numero_caselle) {   // Ques
         tabellone[i].effetto = 0;   // Azzero gli effetti di ogni casella
     }
 
-    int indice_casella = 0;
-    int indice_coda = 0;
-    int effetto = 0, max_effetto = 0, min_effetto = 0;
-
-
     int max_serpenti = 10; //  numero massimo di serpenti
     int min_serpenti = 4;  // numero minimo di serpenti
     int num_serpenti = genera_numero_in_range(min_serpenti, max_serpenti);  // genero randomicamente un numero di serpenti tra min_serpenti e max_serpenti
+    printf("Numero serpenti: %d", num_serpenti);
 
-    for(int i = 0; i < num_serpenti; i++) {
-        indice_casella = genera_numero_in_range(1, indice_casella - 1);  // genero un indice_casella randomico compreso tra 1 e indice_casella - 1 (perché la prima casella è la partenza e l'ultima è la vincita)
-        if (tabellone[indice_casella].effetto == 0) {   // controllo se la casella di indice_casella non ha nessun effetto assegnato
-            max_effetto = -1;   // L'effetto serpente per esistere deve far scendere i giocatori di almeno 1 posizione
-            min_effetto = -indice_casella;  // I giocatori non possono scendere più della grandezza della tabella
+    genera_serpenti(tabellone, numero_caselle, num_serpenti);  // Genero i serpenti sul tabellone
 
+    int max_scale = 10; // numero massimo di scale
+    int min_scale = 4;  // numero minimo di scale
+    int num_scale = genera_numero_in_range(min_scale, max_scale); // genero randomicamente un numero di scale tra min_scale e max_scale
+    printf("\nNumero scale: %d\r\n", num_serpenti);
+
+    genera_scale(tabellone, numero_caselle, num_scale);
+}
+
+void genera_serpenti(struct Casella* tabellone, int numero_caselle, int numero_serpenti) {
+    int indice_casella = 0; // Indice della casella per cui sto generando l'effetto
+    int indice_coda = 0;    // Indice della coda di tabellone[indice_casella]. Ogni serpente e scala ha una coda e una testa
+    int effetto = 0, max_effetto = 0, min_effetto = 0;
+
+    for(int i = 0; i < numero_serpenti; i++) {
+        do {
             do {
+                indice_casella = genera_numero_in_range(1, numero_caselle - 2);  // genero un indice_casella randomico compreso tra 1 e numero_caselle - 2 (perché la prima casella è la partenza e l'ultima è la vincita)
+            } while (tabellone[indice_casella].effetto != 0);   // Genera finché non trovi un posto non occupato da serpenti o scale
+
+            max_effetto = -1;   // L'effetto serpente per esistere deve far scendere i giocatori di almeno 1 posizione
+            min_effetto = -indice_casella;  // Il serpente non può sforare inferiormente il tabellone
+
+
             effetto = genera_numero_in_range(min_effetto, max_effetto); // genero randomicamente il valore dell'effetto tra min_effetto e max_effetto
 
             indice_coda = indice_casella - abs(effetto); // ottengo l'indice della casella della coda del serpente; uso abs(x) perché l'effetto è negativo
 
-            } while (tabellone[indice_coda].effetto != 0);  // Rigenero l'effetto finché l'indice della coda non trovo una coda libera (senza effetti)
+        } while (tabellone[indice_coda].effetto != 0);  // Rigenero un nuovo indice_casella e di conseguenza un indice_coda finché non trovo una coda libera (non occupata da serpenti o scale)
 
-            tabellone[indice_casella].effetto = effetto;    // assegno l'effetto generato
-        }
+        tabellone[indice_casella].effetto = effetto;    // assegno l'effetto generato
     }
 }
+
+void genera_scale(struct Casella* tabellone, int numero_caselle, int numero_scale) {
+    int indice_casella = 0; // Indice della casella per cui sto generando l'effetto
+    int indice_testa = 0;    // Indice della testa di tabellone[indice_casella]. La testa della scala è la posizione tabellone[indice_testa] dove il giocatore arrivera dopo aver salito le scale
+    int effetto = 0, max_effetto = 0, min_effetto = 0;
+
+    for(int i = 0; i < numero_scale; i++) {
+        do {
+            do {
+                indice_casella = genera_numero_in_range(1,  numero_caselle - 2);  // genero un indice_casella randomico compreso tra 1 e numero_caselle - 2 (perché la prima casella è la partenza e l'ultima è la vincita)
+            } while (tabellone[indice_casella].effetto == 0);   // Genera finché non trovi un posto non occupato da serpenti o scale
+
+            max_effetto = numero_caselle - indice_casella;   // La scala non può sforare superiormente la tabella
+            min_effetto = 1;  // La scala per esistere deve avere grandezza almeno 1
+
+
+            effetto = genera_numero_in_range(min_effetto, max_effetto); // genero randomicamente il valore dell'effetto tra min_effetto e max_effetto
+
+            indice_testa = indice_casella + effetto; // ottengo l'indice della casella della testa della scala
+
+        } while (tabellone[indice_testa].effetto != 0);  // Rigenero un nuovo indice_casella e di conseguenza un indice_testa finché non trovo una testa libera (non occupata da serpenti o scale)
+
+        tabellone[indice_casella].effetto = effetto;    // assegno l'effetto generato
+    }
+}
+
 
 int genera_numero_in_range(int min, int max) {
     int numero = (rand() % (max - min + 1)) + min;
 
     return numero;
+}
+
+void stampa_tabellone(struct Casella* tabellone, int numero_caselle) {
+    printf("\nTabellone effetti");
+    for(int i = 0; i < numero_caselle; i++) {   // Stampo il tabellone con partenza che corrisonde a t[0][0] e fine t[10][10]
+        if ((i % 10) == 0) {
+            printf("\n");
+        }
+        printf("[%03+d]", tabellone[i].effetto);
+    }
+    printf("\n\n");
+
+    printf("\nTabellone con numeri delle caselle");
+    for(int i = 0; i < numero_caselle; i++) {   // Stampo il tabellone con partenza che corrisonde a t[0][0] e fine t[10][10]
+        if ((i % 10) == 0) {
+            printf("\n");
+        }
+        printf("[%03d]", tabellone[i].numero);
+    }
 }
