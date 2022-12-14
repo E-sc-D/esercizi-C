ff597c0c7f0d8e82661ee78bf8db9ff3a44861b3
Tommaso De Tommaso <detommaso23@gmail.com>
2022-12-14 20:37:12 +0100

    Updated List library to use Variants

--- RandomTools/list.c
+++ RandomTools/list.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#ifndef DATA_TYPE
-#error DATA_TYPE not defined
-#else
+#include "variant.c"
 
 struct Node
 {
-    DATA_TYPE data;
-
+    var data;
     struct Node *next;
     struct Node *previous;
 };
@@ -17,22 +13,22 @@ struct Iterator
 {
     int current_index;
     struct Node *current_node;
-    //struct Node *next;
-    //struct Node *previous;
 };
 
 struct List
 {
     struct Node *head;
-    struct Iterator iterator;    
+    struct Iterator iterator;
 }
 typedef List;
 
 // MUST BE USED TO INITIALIZE A LIST, otherwise the head pointer might not be NULL
-List new_list()
+List list_new()
 {
     List new_list;
     new_list.head = NULL;
+    new_list.iterator.current_index = -1;
+    new_list.iterator.current_node = NULL;
     return new_list;
 }
 
@@ -42,7 +38,7 @@ List new_list()
 
 // Shouldn't be used outside of the library, sets the iterator to NULL
 // to allow for iterator_forward to work
-void iterator_init(List* list)
+void list_iterator_init(List* list)
 {
     list->iterator.current_index = -1;
     list->iterator.current_node = NULL;
@@ -50,7 +46,7 @@ void iterator_init(List* list)
 
 // Shouldn't be used outside of the library, moves the iterator forward,
 // onto the next item in list. Returns 1 if successful, 0 if not
-int iterator_forward(List* list) 
+int list_iterator_forward(List* list) 
 {
     if(list->iterator.current_node == NULL && list->head != NULL)
     {
@@ -71,7 +67,7 @@ int iterator_forward(List* list)
 
 // Shouldn't be used outside of the library, moves the iterator backward, onto the previous item in list.
 // Returns 1 if successful, 0 if not
-int iterator_backward(List* list) 
+int list_iterator_backward(List* list) 
 {
     if(list->iterator.current_node != NULL && list->iterator.current_node->previous != NULL)
     {
@@ -87,18 +83,18 @@ int iterator_backward(List* list)
 
 // Shouldn't be used outside of the library, gets a node at the position specified.
 // Returns the index-nth node of list, if index is -1, returns the last one. CAN return NULL!
-struct Node * node_get(List* list, int index) 
+struct Node * list_node_get(List* list, int index) 
 {
     if(index == -1)
     {
-        iterator_init(list);
-        while(iterator_forward(list));
+        list_iterator_init(list);
+        while(list_iterator_forward(list));
         return list->iterator.current_node;
     }
     else
     {
-        iterator_init(list);
-        while(iterator_forward(list) && list->iterator.current_index != index);
+        list_iterator_init(list);
+        while(list_iterator_forward(list) && list->iterator.current_index != index);
         if(list->iterator.current_index == index)
             return list->iterator.current_node;
         else
@@ -109,9 +105,9 @@ struct Node * node_get(List* list, int index)
 // Shouldn't be used outside of the library, it removes the node at the position specified, 
 // freeing the memory and then moving the next one at the removed one's place.
 // Returns 1 if successful, 0 if not
-int node_remove(List* list, int index) 
+int list_node_remove(List* list, int index) 
 {
-    struct Node* node_to_remove = node_get(list, index);
+    struct Node* node_to_remove = list_node_get(list, index);
     struct Node* previous_node = node_to_remove->previous; // Can be NULL
     struct Node* next_node = node_to_remove->next; // Can be NULL
     if(node_to_remove != NULL)
@@ -135,9 +131,9 @@ int node_remove(List* list, int index)
 // Shouldn't be used outside of the library, it inserts a node at the position specified,
 // moving the old one after the newly inserted one. Cannot insert at position 0 (first item) or -1 / length - 1 (last item)
 // Returns 1 if successful, 0 if not
-int node_insert(List* list, int index, struct Node* node_to_insert) 
+int list_node_insert(List* list, int index, struct Node* node_to_insert) 
 {
-    struct Node* old_node = node_get(list, index);
+    struct Node* old_node = list_node_get(list, index);
     struct Node* previous_node = old_node->previous; // Can be NULL
     if(node_to_insert != NULL && old_node != NULL && previous_node != NULL)
     {
@@ -157,9 +153,9 @@ int node_insert(List* list, int index, struct Node* node_to_insert)
 // Shouldn't be used outside of the library, it adds a node to the list by getting the last node and changing its pointing address
 // If no last node has been found then the list is of length 0 and the node to add will be set as the list's head
 // Doesn't return anything as it should be impossible for it to fail
-void node_add_back(List* list, struct Node* node_to_add) 
+void list_node_add_back(List* list, struct Node* node_to_add) 
 {
-    struct Node* last_node = node_get(list, -1);
+    struct Node* last_node = list_node_get(list, -1);
     if(node_to_add != NULL && last_node != NULL)
     {
         last_node->next = node_to_add;
@@ -177,9 +173,9 @@ void node_add_back(List* list, struct Node* node_to_add)
 // Shouldn't be used outside of the library, it adds a node to the list by setting it as new head and changing its pointing address
 // If no last node has been found then the list is of length 0 and the node to add will be set as the list's head
 // Doesn't return anything as it should be impossible for it to fail
-void node_add_front(List* list, struct Node* node_to_add)
+void list_node_add_front(List* list, struct Node* node_to_add)
 {
-    struct Node* old_head = node_get(list, 0);
+    struct Node* old_head = list_node_get(list, 0);
     if(node_to_add != NULL && old_head != NULL)
     {
         list->head = node_to_add;
@@ -200,11 +196,11 @@ void node_add_front(List* list, struct Node* node_to_add)
 // ##############################
 
 // Gets the list length
-int get_length(List* list)
+int list_length_get(List* list)
 {
     int length = 0;
-    iterator_init(list);
-    while (iterator_forward(list))
+    list_iterator_init(list);
+    while (list_iterator_forward(list))
         length++;
 
     return length;
@@ -212,23 +208,25 @@ int get_length(List* list)
 
 // Gets the data value of an existing list node
 // Returns the index-nth element of list, if index is -1, returns the last one
-DATA_TYPE get_element(List* list, int index) 
+// Returns a ReturnValue struct containing the requested data and a flag which
+// tells if the data is valid or not.
+var list_element_get(List* list, int index) 
 {
-    struct Node* node = node_get(list, index);
+    var new_variant = var_null;
+    struct Node* node = list_node_get(list, index);
     if(node != NULL)
-        return node->data;
-    else
-        return; // GCC returns a warning for this (find a better way to handle node being NULL)
+        new_variant = var_set_var(node->data);   
+    return new_variant;
 }
 
 // Sets the data value of an existing list node
 // Returns 1 if successful, 0 if not
-int set_element(List* list, int index, DATA_TYPE data) 
+int list_element_set(List* list, int index, var data) 
 {
-    struct Node* node = node_get(list, index);
+    struct Node* node = list_node_get(list, index);
     if(node != NULL)
     {
-        node->data = data;
+        node->data = var_set_var(data);
         return 1;
     }
     else
@@ -240,13 +238,13 @@ int set_element(List* list, int index, DATA_TYPE data)
 // Creates a new node and assigns it the data to insert into the list, then the whole node gets inserted with node_insert()
 // A malloc is here
 // Returns 1 if successful, 0 if not
-int insert_element(List* list, int index, DATA_TYPE data)
+int list_element_insert(List* list, int index, var data)
 {
     struct Node* new_node = malloc(sizeof(struct Node));
     if(new_node == NULL) // malloc can return NULL if there's not memory available
         return 0;
-    new_node->data = data;
-    int returnValue = node_insert(list, index, new_node);
+    new_node->data = var_set_var(data);
+    int returnValue = list_node_insert(list, index, new_node);
     if(!returnValue)
     {
         free(new_node);
@@ -261,98 +259,114 @@ int insert_element(List* list, int index, DATA_TYPE data)
 // Creates a new node and assigns it the data to add into the list, then the whole node gets added with node_add()
 // A malloc is here
 // Returns 1 if successful, 0 if not
-int add_element(List* list, DATA_TYPE data) 
+int list_element_add(List* list, var data) 
 {
     struct Node* new_node = malloc(sizeof(struct Node));
     if(new_node == NULL) // malloc can return NULL if there's not memory available
         return 0;
-    new_node->data = data;
-    node_add_back(list, new_node);
+    new_node->data = var_set_var(data);
+    list_node_add_back(list, new_node);
     // As specified in node_add_back and node_add_front, these methods cannot fail,
     return 1; // so the function immediately returns 1 without doing any checks.
 }
 
 // Alias for add_element(), pushes data into the back of the list
 // Returns 1 if successful, 0 if not
-int push_back(List* list, DATA_TYPE data)
+int list_push_back(List* list, var data)
 {
-    return add_element(list, data);
+    return list_element_add(list, data);
 }
 
 // Pushes data into the front of the list
 // Returns 1 if successful, 0 if not
-int push_front(List* list, DATA_TYPE data)
+int list_push_front(List* list, var data)
 {
     struct Node* new_node = malloc(sizeof(struct Node));
     if(new_node == NULL) // malloc can return NULL if there's not memory available
         return 0;
-    new_node->data = data;
-    node_add_front(list, new_node);
+    new_node->data = var_set_var(data);
+    list_node_add_front(list, new_node);
     // As specified in node_add_back and node_add_front, these methods cannot fail,
     return 1; // so the function immediately returns 1 without doing any checks.
 }
 
+int list_push_at(List* list, int index, var data)
+{
+    list_element_insert(list, index, data);
+}
+
 // Pops data from the back of the list, meaning the last item will be returned
 // and deleted from the list. Returns the DATA_TYPE
-DATA_TYPE pop_back(List* list)
+var list_pop_back(List* list)
 {
-    DATA_TYPE data = get_element(list, -1);
-    node_remove(list, -1);
+    var data = list_element_get(list, -1);
+    list_node_remove(list, -1);
     return data;
 }
 
 // Pops data from the front of the list, meaning the first item will be returned
 // and deleted from the list. Returns the DATA_TYPE
-DATA_TYPE pop_front(List* list)
+var list_pop_front(List* list)
 {
-    DATA_TYPE data = get_element(list, 0);
-    node_remove(list, 0);
+    var data = list_element_get(list, 0);
+    list_node_remove(list, 0);
+    return data;
+}
+
+var list_pop_at(List* list, int index)
+{
+    var data = list_element_get(list, index);
+    list_node_remove(list, index);
     return data;
 }
 
 // Peeks data from the front of the list, meaning the first item will be returned
 // and NOT deleted from the list. Returns the DATA_TYPE
-DATA_TYPE peek_front(List* list)
+var list_peek_front(List* list)
 {
-    DATA_TYPE data = get_element(list, 0);
+    var data = list_element_get(list, 0);
     return data;
 }
 
 // Peeks data from the front of the list, meaning the first item will be returned
 // and NOT deleted from the list. Returns the DATA_TYPE
-DATA_TYPE peek_back(List* list)
+var list_peek_back(List* list)
 {
-    DATA_TYPE data = get_element(list, -1);
+    var data = list_element_get(list, -1);
+    return data;
+}
+
+var list_peek_at(List* list, int index)
+{
+    var data = list_element_get(list, index);
     return data;
 }
 
 // Converts the list to a dynamically allocated static array
 // Can return NULL
-DATA_TYPE * list_to_array(List* list)
+var * list_to_array(List* list)
 {
-    DATA_TYPE * array = malloc(sizeof(DATA_TYPE) * get_length(list));
-    iterator_init(list);
-    while(iterator_forward(list))
+    var *array = malloc(sizeof(var) * list_length_get(list));
+    list_iterator_init(list);
+    while(list_iterator_forward(list))
         array[list->iterator.current_index] = list->iterator.current_node->data;
     return array;
 }
 
 // Converts the list to a dynamically allocated static array
-List list_from_array(int length, DATA_TYPE * array)
+List list_from_array(int length, var *array)
 {
-    List list = new_list();
+    List list = list_new();
     int i;
     for(i = 0; i < length; i++)
-        push_back(&list, array[i]);
+        list_push_back(&list, array[i]);
     return list;
 }
 
 // Removes every item from the list
-void clear(List* list)
+void list_clear(List* list)
 {
-    iterator_init(list);
-    while(iterator_forward(list))
-        node_remove(list, list->iterator.current_index);
+    list_iterator_init(list);
+    while(list_iterator_forward(list))
+        list_node_remove(list, list->iterator.current_index);
 }
-
-#endif
