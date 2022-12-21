b35c1735ff90bc716e409d59284aca37cd8ea548
Leonardo Grimaldi - leonardo.grimaldi2@studio.unibo.it <leonardo.grimaldi2@studio.unibo.it>
2022-12-21 11:50:14 +0100

    few changes

--- Sett9-Es1/main.c
+++ Sett9-Es1/main.c
@@ -58,23 +58,43 @@ void gioca_snakes_and_ladders(int numero_giocatori) {
         que_push(i, coda_giocatori);  // Inserisco i giocatori nella coda
     }
 
-    // Turno
     int indice_giocatore_attuale = 0;
     struct Giocatore* giocatore_attuale = NULL;
-    indice_giocatore_attuale = que_pop(coda_giocatori); // ottengo l'indice del giocatore facendo pop della coda
-    giocatore_attuale = &giocatori[indice_giocatore_attuale]; // assegno a giocatore_attuale il puntatore del giocatore in turno
+int c;
+    //  Turno
+    do {
+        indice_giocatore_attuale = que_pop(coda_giocatori); // ottengo l'indice del giocatore facendo pop della coda
+        giocatore_attuale = &giocatori[indice_giocatore_attuale]; // assegno a giocatore_attuale il puntatore del giocatore in turno
+        printf("\nIndice giocatore attuale: %d", indice_giocatore_attuale);
+        printf("\nId Giocatore Attuale: %d", giocatore_attuale->id);
+        printf("\n  Posizione vecchia: %d", giocatore_attuale->posizione);
 
-    dado = lancia_dado();
-    giocatore_attuale->posizione += dado;    // aumento la posizione del giocatore di dado posti
 
-    if (tabellone[giocatore_attuale->posizione].effetto > 0) {
-        // chiedi domanda
-    } else {
-        giocatore_attuale->posizione += tabellone[giocatore_attuale->posizione].effetto;
+        printf("\nInserisci 'd' per lanciare il dado");
+        while ( (c = getchar()) != '\n' && c != EOF ) { }
+        dado = lancia_dado();
+
+        if ((giocatore_attuale->posizione + dado) >= 99) {
+            //il giocatore ha vinto
+            giocatore_attuale-> posizione = 99;
+            break;
+        }
+
+        giocatore_attuale->posizione += dado;    // aumento la posizione del giocatore di dado posti
+
+        if (tabellone[giocatore_attuale->posizione].effetto > 0) {
+            // chiedi domanda
+        } else {
+            giocatore_attuale->posizione += tabellone[giocatore_attuale->posizione].effetto;
+        }
+
+        printf("\n  Posizione nuova: %d", giocatore_attuale->posizione);
+        que_push(indice_giocatore_attuale, coda_giocatori); // rimetto il giocatore nella coda
+    } while (giocatore_attuale->posizione != 99);
+
+    if (giocatore_attuale->posizione == 99) {   // Verifico la vincita
+        // Vinto
     }
-
-
-
 
 
 
